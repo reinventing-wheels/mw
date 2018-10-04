833419132aaf691ac53ae6d5469ef3ccb8c9cba4
foo <5144598+bar@users.noreply.github.com>
2018-10-04 20:14:48 +0300

    feat: prototype chain instead of mutation

--- src/classes/App.ts
+++ src/classes/App.ts
@@ -8,10 +8,10 @@ const ifMatches = (path: string): MiddlewareWrapper => mw => (ctx, next) =>
   ctx.path.startsWith(path) ? mw(ctx, next) : next()
 
 const inBase = (base: string): MiddlewareWrapper => mw => (ctx, next) => {
-  const { base: baseʹ, path: pathʹ } = ctx, ctxʹ = ctx as any
+  const ctxʹ = Object.create(ctx)
   ctxʹ.base = base
-  ctxʹ.path = pathʹ.slice(base.length - 1)
-  return mw(ctx, () => (ctxʹ.base = baseʹ, ctxʹ.path = pathʹ, next()))
+  ctxʹ.path = ctx.path.slice(base.length - 1)
+  return mw(ctxʹ, next)
 }
 
 export class App<C extends Context> {
