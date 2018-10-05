7d80c9b52cead98bd7dacc3358b92b18a4dcf19e
foo <5144598+bar@users.noreply.github.com>
2018-10-05 22:11:21 +0300

    refactor: minor things

--- src/classes/App.ts
+++ src/classes/App.ts
@@ -1,14 +1,17 @@
 import { ContextConstructor, RequestHandler, MiddlewareWrapper, Middleware, Next } from '../types'
 import { Context } from './Context'
 
-const unpack = <C extends Context>(mwOrApp: Middleware<C> | App<C>) =>
+const chain = (proto: object, ...extensions: any[]) =>
+  Object.assign(Object.create(proto), ...extensions)
+
+const unwrap = <C extends Context>(mwOrApp: Middleware<C> | App<C>) =>
   mwOrApp instanceof App ? mwOrApp.mw : mwOrApp
 
 const ifMatches = (path: string): MiddlewareWrapper => mw => (ctx, next) =>
   ctx.path.startsWith(path) ? mw(ctx, next) : next()
 
 const inBase = (base: string): MiddlewareWrapper => mw => (ctx, next) =>
-  mw(Object.assign(Object.create(ctx), { base, path: ctx.path.slice(base.length) }), next)
+  mw(chain(ctx, { base, path: ctx.path.slice(base.length) }), next)
 
 export class App<C extends Context> {
   readonly handler: RequestHandler
@@ -25,13 +28,13 @@ export class App<C extends Context> {
   }
 
   use(mwOrApp: Middleware<C> | App<C>) {
-    this.mws.push(unpack(mwOrApp))
+    this.mws.push(unwrap(mwOrApp))
     return this
   }
 
   mount(path: string, mwOrApp: Middleware<C> | App<C>) {
     const base = path.endsWith('/') && path.slice(0, -1)
-    const mw = unpack(mwOrApp)
+    const mw = unwrap(mwOrApp)
     this.mws.push(ifMatches(path)(base ? inBase(base)(mw) : mw))
     return this
   }
